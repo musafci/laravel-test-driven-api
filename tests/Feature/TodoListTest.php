03b3199edf732ab41f6a50a497b78e318479df78
Md Abu Musa <33667364+musafci@users.noreply.github.com>
2024-01-02 23:34:29 +0600

    Writing first API test

---
+++ tests/Feature/TodoListTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TodoListTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     * test run command : php artisan test --filter=TodoListTest
+     */
+    public function test_example(): void
+    {
+        // $this->withoutExceptionHandling();
+
+        // preparation [prepare]  Step - 1
+
+        // action [perform]  Step - 2
+        $response = $this->getJson(route('todo.list.index'));
+        // dd($response->json());
+
+
+        // assertion [predict]  Step - 3
+        $this->assertEquals(200, $response->status());
+    }
+}
